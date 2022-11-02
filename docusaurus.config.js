9f38e3ca6ff96680e1f65fbd2bb997e303c80577
CRRogo <rogoza@gmail.com>
2022-11-02 10:10:51 -0400

    Github actions deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,9 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'openpixee', // Usually your GitHub org/user name.
-  projectName: 'openpixee.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: 'pixeeworks', // Usually your GitHub org/user name.
+  projectName: 'pixeeworks.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
