acba1035f453372fc6c03dd7097b2104df744081
Surag <129796490+sip49@users.noreply.github.com>
2024-05-21 00:19:15 +0530

    fixed double quotes
    changed single to double for sake of consistency

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
           anonymizeIP: true,
         },
         googleTagManager: {
-          containerID: 'GTM-TSNFBTV',
+          containerID: "GTM-TSNFBTV",
         },
       }),
     ],
