2ce5927e593ff4d59316bcd966ac3ed916f673b4
Daniel D'Avella <dan.davella@pixee.ai>
2024-08-09 16:01:48 -0400

    Update index page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,11 +5,11 @@ import Link from "@docusaurus/Link";
 export default function HomepageFeatures() {
   const navigateToDocs = (type) => {
     if (type === 1) {
-      window.location = "/installing";
+      window.location = "/intro";
     } else if (type === 2) {
-      window.location = "/codemods/overview";
+      window.location = "/code-scanning-tools/overview";
     } else if (type === 3) {
-      window.location = "https://github.com/pixee/pixee-cli";
+      window.location = "/installing";
     } else if (type === 4) {
       window.location = "/configuring";
     } else if (type === 5) {
@@ -27,64 +27,40 @@ export default function HomepageFeatures() {
       <div className={styles.grid}>
         <div className={styles.item} onClick={() => navigateToDocs(1)}>
           <h1>
-            ⬇ <span>Install Pixeebot</span>
+            👋 <span>Introduction</span>
           </h1>
           <p>
-            Pixeebot installation begins by visiting our{" "}
-            <Link to="https://github.com/apps/pixeebot">GitHub App page</Link>.
-            From there, click the <span>Configure</span> button and follow the
-            prompts from GitHub. You’ll be directed to your Pixee dashboard once
-            the process is complete.
+            Welcome to Pixeebot! Learn how Pixeebot can: <ul>
+             <li>Fix security issues reported by your tools</li>
+             <li>Triage security tool findings</li>
+             <li>Harden your code</li>
+            </ul>
           </p>
         </div>
         <div className={styles.item} onClick={() => navigateToDocs(2)}>
           <h1>
-            🌱 <span>Core codemods</span>
+            🛠️ <span>Supported Tools</span>
           </h1>
           <p>
-            Get detailed information on our core codemods, maintained as part of
-            the <Link to="https://codemodder.io/">codemodder project</Link> to
-            help strengthen your code.
-          </p>
-          <p>
-            <Link to="/category/java">Java</Link>
-          </p>
-          <p>
-            <Link to="/category/python">Python</Link>
+            Learn about the code scanning tools Pixeebot supports and how to
+            give Pixeebot access to your tool findings.
           </p>
         </div>
-        <div className={styles.item} onClick={() => navigateToDocs(4)}>
-          <h1>
-            ⚙️ <span>Preferences</span>
-          </h1>
-          <p>
-            Understand how and where to change Pixeebot preferences, view
-            examples and get details about specific properties.
-          </p>
+        <div className={styles.item} onClick={() => navigateToDocs(3)}>
+           <h1>
+            ⬇ <span>Get Started</span>
+           </h1>
+           <p>
+             Pixeebot is available as a{" "}
+             <Link to="https://github.com/apps/pixeebot">GitHub App</Link>.
+             Install it for free to try it out, or <Link to="https://www.pixee.ai/demo-landing-page">contact us</Link> for a demo and on-prem support.
+           </p>
         </div>
         <div className={styles.item} onClick={() => navigateToDocs(6)}>
           <h1>
             ❓ <span>FAQs</span>
           </h1>
           <p>Addressing common topics such as data handling and use of LLMs.</p>
-        </div>
-        <div className={styles.item} onClick={() => navigateToDocs(7)}>
-          <h1>
-            👤️️ <span>User guide</span>
-          </h1>
-          <p>
-            Learn more about how to engage with Pixeebot, including a summon
-            command to call it whenever you’re ready!
-          </p>
-        </div>
-        <div className={styles.item} onClick={() => navigateToDocs()}>
-          <h1>
-            📄 <span>Release notes</span>
-          </h1>
-          <p>
-            <br /> We are constantly updating our product. Check out our weekly
-            release notes here.
-          </p>
         </div>
       </div>
     </>
